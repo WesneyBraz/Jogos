824f40a9e2bccc2840a66d17b3391faf17ecee96
WesneyBraz <wesney.trajano@gmail.com>
2023-01-25 20:30:59 -0300

    pop-up-3

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function kanto(){
+function kanto() {
     document.getElementById('pokemon-fire-red').style.display = 'block';
 }
-function closeKanto(){
+function hoenn() {
+    document.getElementById('pokemon-ruby').style.display = 'block';
+}
+function closeKanto() {
     document.getElementById('pokemon-fire-red').style.display = 'none';
+}
+function closeHoenn() {
+    document.getElementById('pokemon-ruby').style.display = 'none';
 }
