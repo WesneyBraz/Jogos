ff5a34c9e519b569c155c895860fadb9aa6ac057
WesneyBraz <wesney.trajano@gmail.com>
2023-01-25 17:54:09 -0300

    pop-up-2

---
+++ script.js
@@ -0,0 +1,6 @@
+function kanto(){
+    document.getElementById('pokemon-fire-red').style.display = 'block';
+}
+function closeKanto(){
+    document.getElementById('pokemon-fire-red').style.display = 'none';
+}
