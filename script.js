f27d0f8b6969737d400824a8bf4009e8032f59b8
WesneyBraz <wesney.trajano@gmail.com>
2023-01-26 16:18:17 -0300

    pop-up-fim/pesquisa

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function kanto() {
+function abrirKanto() {
     document.getElementById('pokemon-fire-red').style.display = 'block';
 }
-function hoenn() {
+function abrirHoenn() {
     document.getElementById('pokemon-ruby').style.display = 'block';
 }
-function closeKanto() {
+function abrirJohto(){
+    document.getElementById('pokemon-heart-gold').style.display = 'block';
+}
+function abrirSinnoh(){
+    document.getElementById('pokemon-platinum').style.display = 'block';
+}
+function abrirUnova(){
+    document.getElementById('pokemon-white-2').style.display = 'block';
+}
+function abrirKalos(){
+    document.getElementById('pokemon-y').style.display = 'block';
+}
+
+function fechar() {
     document.getElementById('pokemon-fire-red').style.display = 'none';
-}
-function closeHoenn() {
     document.getElementById('pokemon-ruby').style.display = 'none';
+    document.getElementById('pokemon-heart-gold').style.display = 'none';
+    document.getElementById('pokemon-platinum').style.display = 'none';
+    document.getElementById('pokemon-white-2').style.display = 'none';
+    document.getElementById('pokemon-y').style.display = 'none';
 }
+
